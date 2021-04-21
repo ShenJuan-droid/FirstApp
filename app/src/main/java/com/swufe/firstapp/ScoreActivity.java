20cf71e45d9200e2d05b93a4b2c52cc249afd568
Shenjuan-droid <2590328016@qq.com>
2021-04-21 19:25:02 +0800

    双重计分程序

--- app/src/main/java/com/swufe/firstapp/ScoreActivity.java
+++ app/src/main/java/com/swufe/firstapp/ScoreActivity.java
@@ -1,7 +1,10 @@
 package com.swufe.firstapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.BreakIterator;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,14 +12,16 @@ import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity {
 
-    TextView score;
+    TextView scoreA;
+    TextView scoreB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
 
-        score = (TextView)findViewById(R.id.score);
+        scoreA = (TextView)findViewById(R.id.score);
+        scoreB = (TextView)findViewById(R.id.score1);
 
     }
     public void btnAdd1(View btn){
@@ -28,14 +33,37 @@ public class ScoreActivity extends AppCompatActivity {
     public void btnAdd3(View btn){
         showScore(3);
     }
-    public void btnReset(View btn){
-        score.setText("0");
+
+    public void btnAdd4(View btn){
+        showScore1(1);
     }
+    public void btnAdd5(View btn){
+        showScore1(2);
+    }
+    public void btnAdd6(View btn){
+        showScore1(3);
+    }
+
+
+    public void btnReset(View btn){
+        scoreA.setText("0");
+        scoreB.setText("0");
+    }
+
     private void showScore(int inc){
         Log log = null;
         log.i("show","inc="+inc);
-        String oldScore = (String)score.getText();
+        String oldScore = (String)scoreA.getText();
         int newScore = Integer.parseInt(oldScore) + inc;
-        score.setText(""+newScore);
+        scoreA.setText(""+newScore);
     }
+
+    private void showScore1(int inc){
+        Log log = null;
+        log.i("show","inc="+inc);
+        String oldScore = (String)scoreB.getText();
+        int newScore = Integer.parseInt(oldScore) + inc;
+        scoreB.setText(""+newScore);
+    }
+
 }
