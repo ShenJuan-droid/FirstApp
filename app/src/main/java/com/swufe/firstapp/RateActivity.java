db98db2faf585458aa176aeac340cc465bba827c
Shenjuan-droid <2590328016@qq.com>
2021-04-29 14:43:31 +0800

    处理汇率配置

--- app/src/main/java/com/swufe/firstapp/RateActivity.java
+++ app/src/main/java/com/swufe/firstapp/RateActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class RateActivity extends AppCompatActivity {
+
+    private final String TAG = "Rate";
+    private float dollarRate = 0.1f;
+    private float euroRate = 0.2f;
+    private float wonRate = 0.3f;
 
     EditText rmb;
     TextView result;
@@ -25,23 +32,55 @@ public class RateActivity extends AppCompatActivity {
         result = findViewById(R.id.result);
     }
 
-    public void onclick(View btn){
+    public void onClick(View btn){
         //获取用户输入内容
         String str = rmb.getText().toString();
         float r = 0;
         if(str.length()>0){
             r = Float.parseFloat(str);
+        }else{
+            //用户没有输入内容
+            Toast.makeText(this,"请输入内容",Toast.LENGTH_SHORT).show();
         }
 
+        Log.i(TAG,"onclick:r=" + r);
+
+        //计算
         if(btn.getId()==R.id.btn_dollar){
-            float val = r * 6.7f;
-            result.setText(String.valueOf(val));
+            result.setText(String.format("%.2f,r*dollarRate"));
         }else if(btn.getId() == R.id.btn_euro){
-            float val = r * 11f;
-            result.setText(val + "");
+            result.setText(String.format("%.2f,r*euroRate"));
         }else{
-            float val = r * 500;
-            result.setText(String.valueOf(val));
+            result.setText(String.format("%.2f,r*wonRate"));
         }
+    }
+
+    //打开一个页面Activity
+    public void openOne(View btn){
+        Intent config = new Intent(this,ConfigActivity.class);
+        config.putExtra("dollar_rate_key",dollarRate);
+        config.putExtra("euro_rate_key",euroRate);
+        config.putExtra("won_rate_key",wonRate);
+
+        Log.i(TAG,"openOne:dollarRate=" + dollarRate);
+        Log.i(TAG,"openOne:euroRate=" + euroRate);
+        Log.i(TAG,"openOne:wonRate=" + wonRate);
+
+        //startActivity(config);
+        startActivityForResult(config,1);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==1 && resultCode==2){
+            Bundle bundle = data.getExtras();
+            assert bundle != null;
+            dollarRate = bundle.getFloat("key_dollar",0.1f);
+            euroRate = bundle.getFloat("key_euro",0.1f);
+            wonRate = bundle.getFloat("key_won",0.1f);
+            Log.i(TAG, "onActivityResult: dollarRate=" + dollarRate);
+            Log.i(TAG, "onActivityResult: euroRate=" + euroRate);
+            Log.i(TAG, "onActivityResult: wonRate=" + wonRate);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
