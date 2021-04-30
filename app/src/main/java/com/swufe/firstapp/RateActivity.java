05969f6a9daade947e367aa6e064e15e5ebdcdd0
Shenjuan-droid <2590328016@qq.com>
2021-04-30 11:36:38 +0800

    SharedPerfenced+开启子线程+在子线程中获取网络数据

--- app/src/main/java/com/swufe/firstapp/RateActivity.java
+++ app/src/main/java/com/swufe/firstapp/RateActivity.java
@@ -3,24 +3,39 @@ package com.swufe.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class RateActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-    private final String TAG = "Rate";
-    private float dollarRate = 0.1f;
-    private float euroRate = 0.2f;
-    private float wonRate = 0.3f;
+public class RateActivity extends AppCompatActivity implements Runnable{
+
+    private static final String TAG = "Rate";
+    private float dollarRate = 0.0f;
+    private float euroRate = 0.1266f;
+    private float wonRate = 170.2708f;
 
     EditText rmb;
     TextView result;
+
+    Handler handler;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -30,6 +45,35 @@ public class RateActivity extends AppCompatActivity {
 
         rmb = findViewById(R.id.input_rmb);
         result = findViewById(R.id.result);
+
+        //
+        SharedPreferences sharedPreferences = getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+
+        dollarRate = sharedPreferences.getFloat("dollar_rate",0.0f);
+        euroRate = sharedPreferences.getFloat("euro_rate",0.0f);
+        wonRate = sharedPreferences.getFloat("won_rate",0.0f);
+
+        Log.i(TAG,"onCreate: sp dollarRate" + dollarRate);
+        Log.i(TAG,"onCreate: sp euroRate" + euroRate);
+        Log.i(TAG,"onCreate: sp wonRate" + wonRate);
+
+        //开启线程
+        Thread t = new Thread(this);
+        t.start();
+
+        //创建handler对象
+        handler = new Handler(){
+            public void handleMessage(Message msg){
+                if(msg.what==5){
+                    String str = (String) msg.obj;
+                    Log.i(TAG,"handleMessage:getMessage msg=" + str);
+                    result.setText(str);
+                }
+                super.handleMessage(msg);
+            }
+        };
+
+
     }
 
     public void onClick(View btn){
@@ -79,8 +123,63 @@ public class RateActivity extends AppCompatActivity {
             Log.i(TAG, "onActivityResult: dollarRate=" + dollarRate);
             Log.i(TAG, "onActivityResult: euroRate=" + euroRate);
             Log.i(TAG, "onActivityResult: wonRate=" + wonRate);
+
+            //
+            SharedPreferences sp = getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putFloat("dollar_rate",dollarRate);
+            editor.putFloat("euro_rate",euroRate);
+            editor.putFloat("won_rate",wonRate);
+            editor.apply();
         }
 
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void run(){
+        Log.i(TAG,"run:......");
+        for(int i=1;i<3;i++) {
+            Log.i(TAG,"run:i=" + i);
+            //耗时操作
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //返回消息到主线程
+        Message msg = handler.obtainMessage(5);
+        msg.obj = "Hello from run()";
+        handler.sendMessage(msg);
+
+        //获取网络数据
+        URL url = null;
+        try{
+            url = new URL("Http://www.usd-cny.com/icbc.htm");
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            InputStream in = http.getInputStream();
+
+            String html = inputStream2String(in);
+            Log.i(TAG,"run: html=" + html);
+        }catch(MalformedURLException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private String inputStream2String(InputStream inputStream) throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(inputStream,"UTF-8");
+        for(; ;){
+            int rsz = in.read(buffer,0,buffer.length);
+            if(rsz<0)
+                break;
+            out.append(buffer,0,rsz);
+        }
+        return out.toString();
     }
 }
